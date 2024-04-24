9107ced98f394fd555ac9f7b41e2ebafc146bb4f
Kim Yeongbin <bigstar96115@hanmail.net>
2024-04-24 14:38:07 +0000

    Sync LeetCode submission Runtime - 10 ms (0.53%), Memory - 11.5 MB (100.00%)

---
+++ my-solutions/1236-n-th-tribonacci-number/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def tribonacci(self, n):
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        
+        a, b, c = 0, 1, 1
+        
+        for i in range(3, n + 1):
+            next_trib = a + b + c
+            a = b
+            b = c
+            c = next_trib
+        
+        return c
+
