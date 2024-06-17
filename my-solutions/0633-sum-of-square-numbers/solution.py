26821ce4202a71c82bcb068d3e378e718304f0dd
Kim Yeongbin <bigstar96115@hanmail.net>
2024-06-17 14:33:51 +0000

    Sync LeetCode submission Runtime - 72 ms (22.17%), Memory - 11.6 MB (100.00%)

---
+++ my-solutions/0633-sum-of-square-numbers/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def judgeSquareSum(self, c):
+        a = 0
+        b = int(math.sqrt(c))
+        
+        while a <= b:
+            sum_of_squares = a * a + b * b
+            if sum_of_squares == c:
+                return True
+            elif sum_of_squares < c:
+                a += 1
+            else:
+                b -= 1
+        
+        return False
+            
+        
