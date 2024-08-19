20376bdc9a585f970a3bd45bf8586e30904097ae
Kim Yeongbin <bigstar96115@hanmail.net>
2024-08-19 09:45:15 +0000

    Sync LeetCode submission Runtime - 11 ms (51.19%), Memory - 11.8 MB (100.00%)

---
+++ my-solutions/0650-2-keys-keyboard/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minSteps(self, n):
+        if n == 1:
+            return 0
+
+        steps = 0
+        factor = 2
+        
+        while n > 1:
+            while n % factor == 0:
+                steps += factor
+                n //= factor
+            factor += 1
+        
+        return steps
+
+
+        
