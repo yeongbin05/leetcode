2513895fb601a3a9bd349675d26eb9616c3588cc
Kim Yeongbin <bigstar96115@hanmail.net>
2024-10-25 13:03:04 +0000

    Sync LeetCode submission Runtime - 49 ms (81.36%), Memory - 28.1 MB (16.10%)

--- my-solutions/1350-remove-sub-folders-from-the-filesystem/solution.py
+++ my-solutions/1350-remove-sub-folders-from-the-filesystem/solution.py
@@ -1,10 +1,15 @@
 class Solution:
-    def removeSubfolders(self, folder: List[str]) -> List[str]:
+    def removeSubfolders(self, folder):
+        # 1. 폴더를 사전 순으로 정렬
         folder.sort()
-        res = []
-
-        for i in folder:
-            if not res or not i.startswith(res[-1]+'/'):
-                res.append(i)
-
-        return res
+        
+        # 2. 결과를 저장할 리스트
+        result = []
+        
+        # 3. 폴더를 순회하면서 하위 폴더인지 체크
+        for f in folder:
+            # 결과가 비어 있거나, 현재 폴더가 마지막 결과 폴더의 하위 폴더가 아니면 추가
+            if not result or not f.startswith(result[-1] + '/'):
+                result.append(f)
+        
+        return result
