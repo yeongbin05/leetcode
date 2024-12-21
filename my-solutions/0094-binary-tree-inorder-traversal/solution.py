34f678628b5dfe53f934cc6da75d60bc3ce1cf03
Kim Yeongbin <bigstar96115@hanmail.net>
2024-12-21 11:37:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (36.90%)

--- my-solutions/0094-binary-tree-inorder-traversal/solution.py
+++ my-solutions/0094-binary-tree-inorder-traversal/solution.py
@@ -1,18 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def inorderTraversal(self, root):
-        res = []
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        def dfs(node):
+            if not node :
+                return
 
-        if not root :
-            return res
+            dfs(node.left)
+            ans.append(node.val)
+            dfs(node.right)
 
-        res += self.inorderTraversal(root.left)
-        res.append(root.val)
-        res += self.inorderTraversal(root.right)
+        dfs(root)
 
-        return res
+        return ans
