1234073ee53feb43cfe02d8766016e7943341596
Kim Yeongbin <bigstar96115@hanmail.net>
2025-01-30 08:20:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (54.20%)

--- my-solutions/0782-jewels-and-stones/solution.py
+++ my-solutions/0782-jewels-and-stones/solution.py
@@ -1,11 +1,13 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        ans = 0
         dic = {}
         for i in jewels:
             if i not in dic:
                 dic[i] = 1
-        ans = 0
+
         for i in stones:
             if i in dic:
                 ans += 1
-        return ans
+
+        return ans 
