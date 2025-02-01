316dfbdb5181e73a40cf564f210e2f17927f4c2e
Kim Yeongbin <bigstar96115@hanmail.net>
2025-02-01 03:47:16 +0000

    Sync LeetCode submission Runtime - 3 ms (91.54%), Memory - 21.3 MB (52.04%)

--- my-solutions/0347-top-k-frequent-elements/solution.py
+++ my-solutions/0347-top-k-frequent-elements/solution.py
@@ -1,14 +1,26 @@
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ans = []
+        n = len(nums)
+        if n == k :
+            return nums
+
         dic = {}
+        hq = []
+        # hq 길이
+        cnt = 0
         for i in nums:
-            if i not in dic:
-                dic[i] = 1
-            else:
+            if i in dic:
                 dic[i] += 1
+            else:
+                dic[i] = 1
+        for i in dic:
+            heapq.heappush(hq,(dic[i],i))
+            cnt += 1
+            if cnt > k:
+                heapq.heappop(hq)
+            
+
+        return [i[1] for i in hq]
+
         
-        dic = sorted(dic,key = lambda x : -dic[x])
-        for i in range(k):
-            ans.append(dic[i])
-        return ans
