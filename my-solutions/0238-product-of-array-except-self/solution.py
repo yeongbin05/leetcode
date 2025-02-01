fca28d3240177175931223d87816c2df8adc65f2
Kim Yeongbin <bigstar96115@hanmail.net>
2025-02-01 04:23:37 +0000

    Sync LeetCode submission Runtime - 20 ms (73.24%), Memory - 23.1 MB (91.46%)

--- my-solutions/0238-product-of-array-except-self/solution.py
+++ my-solutions/0238-product-of-array-except-self/solution.py
@@ -1,27 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        dic = {}
-        ans = []
-        for i in nums :
-            if i in dic:
-                dic[i] += 1
-            else :
-                dic[i] = 1
-        
-        for i in nums:
-            if dic[i] == 1:
-                del dic[i]
-            else :
-                dic[i] -= 1
-            cnt = 1
-            print(dic)
-            for j in dic:
-                cnt *= j ** dic[j]
-            ans.append(cnt)
-            if i not in dic :
-                dic[i] = 1
-            else :
-                dic[i] += 1
-            
+        n = len(nums)
+        ans = [1] * n
+        left = right = 1
+        for i in range(n):
+            ans[i] *= left
+            left *= nums[i]
+
+        for i in range(n-1,-1,-1):
+            ans[i] *= right
+            right *= nums[i]
 
         return ans
