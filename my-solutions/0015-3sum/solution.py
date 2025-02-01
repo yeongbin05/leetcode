97725ff2f8719b35d88266291404ab26d6e873ee
Kim Yeongbin <bigstar96115@hanmail.net>
2025-02-01 06:44:27 +0000

    Sync LeetCode submission Runtime - 583 ms (55.28%), Memory - 20.6 MB (53.14%)

--- my-solutions/0015-3sum/solution.py
+++ my-solutions/0015-3sum/solution.py
@@ -2,24 +2,16 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         ans = []
-        len_nums = len(nums)
-        for i in range(len_nums):
-            if i > 0 and nums[i] == nums[i - 1]:  # 중복 건너뛰기
+        n = len(nums)
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            start, end = i + 1, len_nums - 1
-         
+            start ,end = i+1,n-1
 
-            while start < end:
-                total = nums[i] + nums[start] + nums[end]
-                
-                if total < 0:
-                    start += 1
-                elif total > 0:
-                    end -= 1
-                else:
-                    ans.append([nums[i], nums[start], nums[end]])
-                    
-                    # 중복 건너뛰기
+            while end > start:
+                temp = nums[i] + nums[start] + nums[end]
+                if temp == 0 :
+                    ans.append([nums[i], nums[start],nums[end]])
                     while start < end and nums[start] == nums[start + 1]:
                         start += 1
                     while start < end and nums[end] == nums[end - 1]:
@@ -27,6 +19,10 @@ class Solution:
                     
                     start += 1
                     end -= 1
+                elif temp > 0 :
+                    end -= 1
+                else :
+                    start += 1
 
 
         return ans
