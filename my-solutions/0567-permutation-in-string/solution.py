f4379a904576374c3f679e4d1f5b4b260d6e5620
Kim Yeongbin <bigstar96115@hanmail.net>
2025-02-03 03:43:04 +0000

    Sync LeetCode submission Runtime - 11 ms (97.22%), Memory - 17.9 MB (41.94%)

--- my-solutions/0567-permutation-in-string/solution.py
+++ my-solutions/0567-permutation-in-string/solution.py
@@ -1,32 +1,22 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        len_s1 = len(s1)
-        len_s2 = len(s2)
-
-        if len_s1 > len_s2:
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        len_s1,len_s2 = len(s1),len(s2)
+        if len_s1 > len_s2 :
             return False
-        
-        # s1의 문자 빈도수를 저장하는 딕셔너리 생성
-        dic1 = {}
-        for i in s1:
-            dic1[i] = dic1.get(i, 0) + 1
-        
-        # s2에서 슬라이딩 윈도우 초기화
-        dic2 = {}
+        s1_cnt = [0] * 26
+        s2_cnt = [0] * 26
         for i in range(len_s1):
-            dic2[s2[i]] = dic2.get(s2[i], 0) + 1
-        
-        # 윈도우를 하나씩 옮기면서 비교
-        for i in range(len_s2 - len_s1):
-            if dic1 == dic2:
+            s1_cnt[ord(s1[i]) - ord('a')] += 1
+            s2_cnt[ord(s2[i]) - ord('a')] += 1
+
+        if s1_cnt == s2_cnt:
+            return True
+
+        for i in range(len_s2-len_s1):
+            s2_cnt[ord(s2[i+len_s1])-ord('a')]+= 1
+            s2_cnt[ord(s2[i])-ord('a')]-= 1
+            if s1_cnt == s2_cnt:
                 return True
-            # 윈도우의 왼쪽 문자 제거
-            dic2[s2[i]] -= 1
-            if dic2[s2[i]] == 0:
-                del dic2[s2[i]]
-            # 윈도우의 오른쪽 문자 추가
-            dic2[s2[i + len_s1]] = dic2.get(s2[i + len_s1], 0) + 1
-        
-        # 마지막 윈도우 체크
-        return dic1 == dic2
+
+        return False
 
