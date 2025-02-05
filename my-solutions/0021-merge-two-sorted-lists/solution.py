8ec324258cfc7d8711f2d8dc60cdc5d419b2b001
Kim Yeongbin <bigstar96115@hanmail.net>
2025-02-05 06:25:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (26.94%)

--- my-solutions/0021-merge-two-sorted-lists/solution.py
+++ my-solutions/0021-merge-two-sorted-lists/solution.py
@@ -5,10 +5,17 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        if (not l1) or (l2 and l1.val > l2.val) :
-            l1, l2 = l2, l1
+        graph = ListNode()
+        tail = graph
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        tail.next = l1 if l1 else l2
 
-        if l1:
-            l1.next = self.mergeTwoLists(l1.next,l2)
 
-        return l1
+        return graph.next
