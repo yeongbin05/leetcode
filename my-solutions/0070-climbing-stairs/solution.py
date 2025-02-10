f33c613af22f03508334714cf414d46d29afdfd3
Kim Yeongbin <bigstar96115@hanmail.net>
2025-02-10 06:19:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (53.60%)

--- my-solutions/0070-climbing-stairs/solution.py
+++ my-solutions/0070-climbing-stairs/solution.py
@@ -2,10 +2,13 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
+        elif n == 2:
+            return 2
         dp = [0] * (n+1)
         dp[1] = 1
         dp[2] = 2
         for i in range(3,n+1):
-            dp[i] = dp[i-2] + dp[i-1] 
-        print(dp)
+            dp[i] = dp[i-1] + dp[i-2]
+
         return dp[n]
+
