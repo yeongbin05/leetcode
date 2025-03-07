5764334f6e1f2e517a4787e17225ea9e7c706e83
Kim Yeongbin <bigstar96115@hanmail.net>
2025-03-07 04:16:18 +0000

    Sync LeetCode submission Runtime - 6 ms (52.74%), Memory - 18.5 MB (40.70%)

---
+++ my-solutions/0338-counting-bits/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
+        dp = [0] * (n+1)
+        dp[1] = 1
+        for i in range(2,n+1):
+            # 홀수이면 
+            if i % 2 == 0 :
+                dp[i] = dp[i//2]
+            else:
+                dp[i] = dp[(i-1)//2] + 1
+        
+        return dp
