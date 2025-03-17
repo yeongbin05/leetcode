ad3780b3fe8231864ae978b22dc3cc08ff743287
Kim Yeongbin <bigstar96115@hanmail.net>
2025-03-17 11:45:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.1 MB (30.03%)

--- my-solutions/0344-reverse-string/solution.py
+++ my-solutions/0344-reverse-string/solution.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        a = s[::-1]
-        for i in range(len(s)):
-            s[i] = a[i]
+        n = len(s)
+        for i in range(n//2):
+            s[i],s[n-1-i] = s[n-1-i],s[i]
+        
