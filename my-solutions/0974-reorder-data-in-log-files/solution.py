b36b774f115dd12bb9bb54e2830ccc6f607cc8f7
Kim Yeongbin <bigstar96115@hanmail.net>
2025-03-17 11:55:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (30.06%)

--- my-solutions/0974-reorder-data-in-log-files/solution.py
+++ my-solutions/0974-reorder-data-in-log-files/solution.py
@@ -1,20 +1,12 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        letter_log = []
-        digit_log = []
-        nums = ['1','2','3','4','5','6','7','8','9','0']
+        letter = []
+        digit = []
         for i in logs:
             temp = i.split()
-            print(temp)
-            for j in temp[1:]:
-                if j.isnumeric():
-                    digit_log.append(i)
-                    break
-                else:
-                    letter_log.append(i)
-                    break
-
-       
-
-        letter_log = sorted(letter_log,key=lambda x: (x.split()[1:],x.split()[0]) )
-        return letter_log + digit_log
+            if temp[1].isdigit():
+                digit.append(i)
+            else:
+                letter.append(i)
+        letter.sort(key= lambda x:(x.split()[1:],x.split()[0]))
+        return letter + digit
