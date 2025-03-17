ef7bc0f88cccd6a3df5c09de82bbb6984ce02c9c
Kim Yeongbin <bigstar96115@hanmail.net>
2025-03-17 14:41:39 +0000

    Sync LeetCode submission Runtime - 4 ms (26.97%), Memory - 17.8 MB (34.78%)

--- my-solutions/0837-most-common-word/solution.py
+++ my-solutions/0837-most-common-word/solution.py
@@ -1,44 +1,17 @@
+import re
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        paragraph = [_ for _ in paragraph]
-        for i in range(len(paragraph)):
-            if not paragraph[i].isalnum():
-                paragraph[i] = ''
-        
-        paragraphs = ''
-        temp =  ''
-        flag = True
-        for i in paragraph:
-            flag = True
-            if i != '' :
-                temp += i
-            else :
-                paragraphs += temp
-                paragraphs += ' ' 
-                
-                temp = ''
-                flag = False
-        if flag == True:
-            paragraphs += temp
         dic = {}
-        print(paragraphs,'paragraphs1')
-        paragraphs = paragraphs.split()
-        print(paragraph,'paragraph')
-        print(paragraphs,'paragraphs')
-        for i in paragraphs:
-            i = i.lower()
+        paragraph = re.sub(r'[!?,;.\']',' ',paragraph).lower().split()
+
+        cnt = 0
+        for i in paragraph:
             if i not in banned:
                 if i in dic:
                     dic[i] += 1
                 else:
                     dic[i] = 1
-        cnt = 0
-        ans = ''
-        print(dic)
-        for i in dic :
-            if dic[i] > cnt :
-                ans = i
-                cnt = dic[i]
-
+                if dic[i] > cnt:
+                    cnt += 1
+                    ans = i
         return ans
-
