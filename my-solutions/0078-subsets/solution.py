1d7465a838400d8288ebeb420a89c22e26767d2b
Kim Yeongbin <bigstar96115@hanmail.net>
2025-03-20 15:46:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (61.66%)

--- my-solutions/0078-subsets/solution.py
+++ my-solutions/0078-subsets/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def subset(idx):
-            ans.append(list(temp))
-            for i in range(idx,n):
-                temp.append(nums[i])
-                subset(i+1)
-                temp.pop()
-
-
-        temp = []
         ans = []
         n = len(nums)
-        subset(0)
+        def comb(idx,temp):
+            if len(temp) <= n:
+                ans.append(temp[:])
+
+            for i in range(idx,n):
+                temp.append(nums[i])
+                comb(i+1,temp)
+                temp.pop()
+
+        comb(0,[])
         return ans
