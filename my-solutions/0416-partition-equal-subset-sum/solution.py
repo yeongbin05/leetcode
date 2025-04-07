e239af10c1437e2dbb516952a24848b81918de32
Kim Yeongbin <bigstar96115@hanmail.net>
2025-04-07 01:35:56 +0000

    Sync LeetCode submission Runtime - 471 ms (79.32%), Memory - 17.9 MB (74.06%)

---
+++ my-solutions/0416-partition-equal-subset-sum/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+        target = total // 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+
+        for num in nums:
+            for j in range(target, num - 1, -1):
+                if dp[j - num]:
+                    dp[j] = True
+        return dp[target]
