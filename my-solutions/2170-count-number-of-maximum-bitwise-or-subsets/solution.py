0a7f33efb38f04acc051f346b5ce98b34d279a25
Kim Yeongbin <bigstar96115@hanmail.net>
2025-07-28 04:48:41 +0000

    Sync LeetCode submission Runtime - 211 ms (77.43%), Memory - 18 MB (24.54%)

--- my-solutions/2170-count-number-of-maximum-bitwise-or-subsets/solution.py
+++ my-solutions/2170-count-number-of-maximum-bitwise-or-subsets/solution.py
@@ -1,25 +1,25 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        max_or = 0
-        for num in nums:
-            max_or |= num
+        n = len(nums)
+        maximum = 0
+        ans = 0
+        def back(temp, idx):
+            nonlocal ans
+            if idx == n:  
+                if temp == maximum:
+                    ans += 1
+                return
 
-        # 결과를 저장할 변수입니다.
-        count = 0
+            # 1) 현재 nums[idx] 포함
+            back(temp | nums[idx], idx + 1)
+            # 2) 현재 nums[idx] 제외
+            back(temp, idx + 1)
 
-        # 백트래킹 함수 정의
-        def backtrack(index, current_or):
-            nonlocal count
-            # 부분 집합의 비트 OR 값이 최대 OR 값과 같으면 카운트를 증가시킵니다.
-            if current_or == max_or:
-                count += 1
-            
-            # index부터 시작해 나머지 원소들을 백트래킹합니다.
-            for i in range(index, len(nums)):
-                # 현재 원소를 추가하여 다음으로 진행합니다.
-                backtrack(i + 1, current_or | nums[i])
+        for i in range(n):
+            maximum |= nums[i]
+        
+        # 첫 인자 : 현재 원소까지 OR값 , 두 번째 인자 : idx값
+        back(0,0)
 
-        # 백트래킹을 시작합니다.
-        backtrack(0, 0)
 
-        return count
+        return ans
