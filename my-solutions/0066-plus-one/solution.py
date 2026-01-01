f659be570501b1716a4f29ec0055a7e51dfdcdd1
Kim Yeongbin <bigstar96115@hanmail.net>
2026-01-01 14:00:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.3 MB (53.30%)

--- my-solutions/0066-plus-one/solution.py
+++ my-solutions/0066-plus-one/solution.py
@@ -1,3 +1,19 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        return list(map(int,str(int(''.join(map(str,digits)))+1)))
+        len_digit = len(digits)
+        idx = len_digit - 1
+        for _ in range(len_digit):
+            if digits[idx] != 9 :
+                digits[idx] += 1
+                break
+
+            else:
+                digits[idx] = 0
+                idx -= 1
+
+        if digits[0] == 0:
+            digits = [1] + digits
+        
+        
+        
+        return digits
