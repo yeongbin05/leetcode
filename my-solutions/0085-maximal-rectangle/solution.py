ab35f2a6fe09b13d316ce43b9d62ae33335c54ad
Kim Yeongbin <bigstar96115@hanmail.net>
2026-01-11 23:18:53 +0000

    Sync LeetCode submission Runtime - 503 ms (7.22%), Memory - 24.7 MB (6.51%)

--- my-solutions/0085-maximal-rectangle/solution.py
+++ my-solutions/0085-maximal-rectangle/solution.py
@@ -1,41 +1,27 @@
-class Solution(object):
-    def maximalRectangle(self, matrix):
-        
-        if not matrix or not matrix[0]:
-            return 0
-
-        rows, cols = len(matrix), len(matrix[0])
-        heights = [0] * cols
-        max_area = 0
-
-        for i in range(rows):
-            for j in range(cols):
-                # Update the height of the histogram
-                if matrix[i][j] == "1":
-                    heights[j] += 1
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        n,m = len(matrix),len(matrix[0])
+        max_width = [[0]*m for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == '0':
+                    max_width[i][j] = 0
                 else:
-                    heights[j] = 0
-
-            max_area = max(max_area, largestRectangleArea(heights))
-
-        return max_area
-
-def largestRectangleArea(heights):
-    stack = []
-    max_area = 0
-    heights.append(0)  # Append a zero at the end to handle the end of the list
-
-    for i in range(len(heights)):
-        while stack and heights[i] < heights[stack[-1]]:
-            # Pop the top of the stack and calculate the area
-            height = heights[stack.pop()]
-            width = i if not stack else i - stack[-1] - 1
-            max_area = max(max_area, height * width)
-
-        stack.append(i)
-
-    return max_area
-
-
-
+                    max_width[i][j] = 1 if j == 0 else max_width[i][j-1] + 1
         
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                if max_width[i][j] == 0:
+                    continue
+                row = i
+                height = 1
+                min_width = max_width[row][j]
+                while row >= 0 and max_width[row][j] > 0 :         
+                    min_width = min(min_width,max_width[row][j])
+                    ans = max(ans,height * min_width)
+                    height += 1
+                    row -= 1
+                
+
+        return ans
