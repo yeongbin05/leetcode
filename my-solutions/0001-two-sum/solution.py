2ae2f40ab74269d1aa3c73f371eef5e34f2c7818
Kim Yeongbin <bigstar96115@hanmail.net>
2026-02-10 06:53:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.7 MB (16.22%)

--- my-solutions/0001-two-sum/solution.py
+++ my-solutions/0001-two-sum/solution.py
@@ -1,10 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic = {}
-        n = len(nums)
-        for i in range(n):
-            if target - nums[i] in dic:
-                return [dic[target-nums[i]],i]
-            
-            else :
-                dic[nums[i]] = i
+        for idx,num in enumerate(nums):
+            if target - num in dic:
+                return [idx,dic[target-num]]
+            if num not in dic:
+                dic[num] = idx
