dc2e0a8fb2bd5548625c2cf98155572b38727f02
Kim Yeongbin <bigstar96115@hanmail.net>
2026-02-10 07:16:38 +0000

    Sync LeetCode submission Runtime - 261 ms (28.62%), Memory - 21.6 MB (84.03%)

--- my-solutions/0200-number-of-islands/solution.py
+++ my-solutions/0200-number-of-islands/solution.py
@@ -1,29 +1,26 @@
 from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+        # bfs , visited체크
+        m , n = len(grid),len(grid[0])
+        visited = [[0]*n for _ in range(m)]
+        ans = 0
         def bfs(row,col):
-            visited[row][col] = True
             q = deque([(row,col)])
+            visited[row][col] =  1
             while q:
                 ci,cj = q.popleft()
-                for di,dj in ((0,1),(0,-1),(1,0),(-1,0)) :
-                    ni = ci + di
-                    nj = cj + dj
-                
-                    if 0<=ni<m and 0<=nj<n and not visited[ni][nj]  and grid[ni][nj] == "1":
-                        q.append((ni,nj))
-                        visited[ni][nj] = True
+                for di,dj in ((0,1),(0,-1),(1,0),(-1,0)):
+                    ni,nj = ci+di, cj+dj
+                    if 0<=ni<m and 0<=nj<n and not visited[ni][nj] and grid[ni][nj] == "1":
+                        visited[ni][nj] = 1
+                        q.append((ni, nj))
 
-        visited = [[False]*n for _ in range(m)]
-        
-        ans = 0
-        for i in range(m):
-            for j in range(n):
-                if not visited[i][j]  and grid[i][j] == "1":
-                    bfs(i,j)
+
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] == "1" and not visited[row][col]:
+                    bfs(row,col)
                     ans += 1
-                    
-        
         return ans
+
