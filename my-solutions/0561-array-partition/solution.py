b07931874163571966de13a54643ae258133db78
Kim Yeongbin <bigstar96115@hanmail.net>
2026-02-12 02:12:39 +0000

    Sync LeetCode submission Runtime - 31 ms (26.95%), Memory - 21.5 MB (80.13%)

--- my-solutions/0561-array-partition/solution.py
+++ my-solutions/0561-array-partition/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        # nums.sort()
-        # ans = 0
-        # for i in range(0,len(nums),2):
-        #     ans += nums[i]
+        nums.sort()
+        ans = 0
+        n = len(nums)
+        for i in range(0,n,2):
+            ans += nums[i]
 
-        # return ans
-        return sum(sorted(nums)[::2])
+        return ans
