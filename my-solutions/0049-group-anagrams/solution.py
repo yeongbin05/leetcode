dcdcb4307957f4f4adf15e317a4e6e5e7a5e0457
Kim Yeongbin <bigstar96115@hanmail.net>
2026-02-22 14:33:41 +0000

    Sync LeetCode submission Runtime - 12 ms (59.26%), Memory - 22.1 MB (74.99%)

--- my-solutions/0049-group-anagrams/solution.py
+++ my-solutions/0049-group-anagrams/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dic = {}
-        for i in strs:
-            if ''.join(sorted(i)) in dic:
-                dic[''.join(sorted(i))].append(i)
-            else:
-                dic[''.join(sorted(i))] = [i]
-
         ans = []
+        for word in strs:
+            sorted_word =  ''.join(sorted(word))
+            print(word)
+            if sorted_word in dic:
+                dic[sorted_word].append(word)
+            else:
+                dic[sorted_word] = [word]
+            
         for i in dic:
             ans.append(dic[i])
 
