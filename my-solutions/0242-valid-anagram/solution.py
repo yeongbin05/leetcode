c8d5cce0e9170e7813dd94fa7e4fbfa2fb113f55
Kim Yeongbin <bigstar96115@hanmail.net>
2026-02-28 00:26:27 +0000

    Sync LeetCode submission Runtime - 12 ms (54.33%), Memory - 19.3 MB (87.43%)

--- my-solutions/0242-valid-anagram/solution.py
+++ my-solutions/0242-valid-anagram/solution.py
@@ -1,22 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        len_s,len_t = len(s),len(t)
-        if len_s != len_t :
-            return False
-        dic_s = {}
-        dic_t = {}
-        for i in range(len_s):
-            if s[i] in dic_s:
-                dic_s[s[i]] += 1
-            else:
-                dic_s[s[i]] = 1
-            if t[i] in dic_t:
-                dic_t[t[i]] += 1
-            else:
-                dic_t[t[i]] = 1
+        count = [0] * 26
+        for i in s:
+            count[ord(i)-ord('a')] += 1
         
-        if dic_s == dic_t :
-            return True
+        for i in t:
+            count[ord(i)-ord('a')] -= 1
+            if count[ord(i)-ord('a')] < 0:
+                return False
+        
+        for i in count:
+            if i != 0:
+                return False
 
-        else:
-            return False
+        return True
