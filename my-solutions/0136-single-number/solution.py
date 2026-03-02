50f0825ea06405db165dcef862d23f4028c926d9
Kim Yeongbin <bigstar96115@hanmail.net>
2026-03-02 06:39:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21 MB (97.14%)

--- my-solutions/0136-single-number/solution.py
+++ my-solutions/0136-single-number/solution.py
@@ -1,10 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        temp = nums[0]
-        for i in range(1,n):
-            temp ^= nums[i]
-
-        return temp
+        ans = 0
+        for i in nums:
+            ans ^= i
+        
+        return ans
