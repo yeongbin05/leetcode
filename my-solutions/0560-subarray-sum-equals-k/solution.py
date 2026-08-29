13758a7889f4e1eb3b87c5e3e1daf5bad36499c7
Kim Yeongbin <bigstar96115@hanmail.net>
2026-08-29 05:30:03 +0000

    Sync LeetCode submission Runtime - 42 ms (19.41%), Memory - 21.8 MB (56.43%)

--- my-solutions/0560-subarray-sum-equals-k/solution.py
+++ my-solutions/0560-subarray-sum-equals-k/solution.py
@@ -1,15 +1,20 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
         ans = 0
-        seen = {0 : 1}
+        pre = [0] * (n+1)
+        for i in range(1,n+1):
+            pre[i] = pre[i-1] + nums[i-1]
 
-        temp = 0
-        for i in nums:
-            temp += i
-            if temp - k in seen:
-                ans += seen[temp-k]
-            if temp in seen:
-                seen[temp] += 1
+        
+        frequency = {0:1,}
+        for i in range(1,n+1):
+            if pre[i]-k in frequency:
+                ans += frequency[pre[i]-k]
+            if pre[i] in frequency:
+                frequency[pre[i]] += 1
             else:
-                seen[temp] = 1
+                frequency[pre[i]] = 1
         return ans
+                
+
