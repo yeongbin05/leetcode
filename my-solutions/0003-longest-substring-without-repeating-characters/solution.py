f6c231d081d259b7e5f184022bcec4604f50a2ba
Kim Yeongbin <bigstar96115@hanmail.net>
2026-09-01 14:21:00 +0000

    Sync LeetCode submission Runtime - 203 ms (47.64%), Memory - 20 MB (24.35%)

--- my-solutions/0003-longest-substring-without-repeating-characters/solution.py
+++ my-solutions/0003-longest-substring-without-repeating-characters/solution.py
@@ -3,17 +3,21 @@ class Solution:
         n = len(s)
         if n == 0:
             return 0
-        
-        dic = {}  # 문자 위치 저장
-        left = 0  # 현재 부분 문자열의 시작 위치
-        ans = 0   # 최장 길이 저장
-        
-        for right in range(n):
-            if s[right] in dic:
-                left = max(left, dic[s[right]] + 1)  # left를 뒤로 이동
-            
-            dic[s[right]] = right  # 현재 문자 위치 갱신
-            ans = max(ans, right - left + 1)  # 최장 길이 갱신
-        
+        cnt = {}
+        ans,left,right = 0,0,0
+
+        while right < n:
+            if s[right] not in cnt:
+                cnt[s[right]] = right
+                right += 1
+                
+            else:
+                left = max(left,cnt[s[right]] + 1)
+                cnt[s[right]] = right
+                right += 1
+            ans = max(ans,right - left)
+
         return ans
+
+
 
