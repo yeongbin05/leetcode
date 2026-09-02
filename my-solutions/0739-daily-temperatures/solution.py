e01e2beeebbc077fef3aa96316b49ab1bf1391d3
Kim Yeongbin <bigstar96115@hanmail.net>
2026-09-02 09:20:52 +0000

    Sync LeetCode submission Runtime - 179 ms (5.03%), Memory - 36.5 MB (5.46%)

--- my-solutions/0739-daily-temperatures/solution.py
+++ my-solutions/0739-daily-temperatures/solution.py
@@ -1,13 +1,15 @@
+from collections import deque
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []
         n = len(temperatures)
+        q = deque()
         ans = [0] * n
-        for idx,val in enumerate(temperatures):
-            while stack and temperatures[stack[-1]] < val:
-                ans[stack[-1]] = idx - stack[-1]
-                stack.pop()
-            
-            stack.append(idx)
+        for i in range(n):
+            while q and q[-1][0] < temperatures[i]:
+                ans[q[-1][1]] = i - q[-1][-1]
+                q.pop()
+
+            q.append([temperatures[i],i])
+
 
         return ans
