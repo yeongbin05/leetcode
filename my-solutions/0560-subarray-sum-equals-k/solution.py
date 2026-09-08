032dd1f3dbabe8eaf61e5aeb0a4555a1747f3c3a
Kim Yeongbin <bigstar96115@hanmail.net>
2026-09-08 09:45:47 +0000

    Sync LeetCode submission Runtime - 43 ms (17.94%), Memory - 22 MB (26.01%)

--- my-solutions/0560-subarray-sum-equals-k/solution.py
+++ my-solutions/0560-subarray-sum-equals-k/solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
+        dic = {0:1}
+        pre = [0] * (n+1)
         ans = 0
-        prefix = [0] * (n+1)
-        frequency = {}
-        for i in range(n):
-            prefix[i+1] = prefix[i] + nums[i]
-        
-        for i in range(n+1):
-            if (prefix[i] - k ) in frequency:
-                ans += frequency[prefix[i]-k]
-            if prefix[i] in frequency:
-                frequency[prefix[i]] += 1
+        for i in range(1,n+1):
+            pre[i] = pre[i-1] + nums[i-1]
+            if pre[i]-k in dic:
+                ans += dic[pre[i]-k]
+
+            if pre[i] in dic:
+                dic[pre[i]]  += 1
             else:
-                frequency[prefix[i]] = 1
+                dic[pre[i]] = 1
+
         return ans
