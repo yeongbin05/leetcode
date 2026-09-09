24da9e5f915fa912f48e8a3c2f7d8362b603c141
Kim Yeongbin <bigstar96115@hanmail.net>
2026-09-09 11:57:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (62.20%)

--- my-solutions/0198-house-robber/solution.py
+++ my-solutions/0198-house-robber/solution.py
@@ -1,9 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [0] * (n+1)
-        for i in range(1,n+1):
-            dp[i] = max(dp[i-1],dp[i-2]+nums[i-1])
+        if n <3 :
+            return max(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+
+        for i in range(2,n):
+            dp[i] = max(dp[i-2] + nums[i],dp[i-1])
 
         return dp[-1]
-        
